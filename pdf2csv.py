3fc84eda8c2aaf950afa08ee2189c2dca29669a6
innersea <innersea@innersea.com>
2023-03-15 00:21:37 +0800

    pdf2csv 增加命令行解析，和进度条

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,22 +1,24 @@
+import argparse
 import pdfplumber
 import csv
+from tqdm import tqdm
 
 
 def pdf2csv(pdf_file, csv_file):
-    # 使用pdfplumber打开pdf文件
     with pdfplumber.open(pdf_file) as pdf, open(csv_file, 'w') as c:
         writer = csv.writer(c)
-        # 遍历pdf文件中的每一页
-        for page in pdf.pages:
-            # 提取每一页中的表格
-            tables = page.extract_tables()
-            # 遍历每一页中的每一个表格
-            for table in tables:
-                # 将表格保存为csv文件
+        for page in tqdm(pdf.pages):
+            for table in page.extract_tables():
                 writer.writerows(table)
+            page.flush_cache() # 清空缓存，否则页面过多会直接拉爆内存。
 
 
 if __name__ == '__main__':
-    pdf = "1.pdf"
+    # pdf = 'z:/1.pdf'
+    parser = argparse.ArgumentParser(
+                    prog='pdf2csv.py',
+                    description='Extract tables in pdf file and save as csv file',
+                    epilog='InnerSea at 2023-3-14')
+    parser.add_argument('pdf', help='pdf file path')
+    pdf = parser.parse_args().pdf
     pdf2csv(pdf, f"{pdf}.csv")
-    print('done')
