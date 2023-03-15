335c9934d5d8e3b947607de07092756fd97ff1d6
innersea <innersea@innersea.com>
2023-03-15 23:15:02 +0800

    拆成两个函数方便复用

--- pdf2csv.py
+++ pdf2csv.py
@@ -1,24 +1,33 @@
+# *--codeing: utf-8--*
+
 import argparse
-import pdfplumber
 import csv
+import pdfplumber
 from tqdm import tqdm
 
 
-def pdf2csv(pdf_file, csv_file):
-    with pdfplumber.open(pdf_file) as pdf, open(csv_file, 'w') as c:
+def pdf_to_table_list(pdf_file) -> list[list]:
+    with pdfplumber.open(pdf_file) as pdf:
+        tables = []
+        for page in tqdm(pdf.pages, desc="Extracting tables in PDF: "):
+            tables.extend(page.extract_tables())
+            page.flush_cache()  # 清空缓存，否则页面过多会直接拉爆内存。
+        return tables
+
+
+def table_list_to_csv(csv_file, tables: list[list]) -> None:
+    with open(csv_file, 'w') as c:
         writer = csv.writer(c)
-        for page in tqdm(pdf.pages):
-            for table in page.extract_tables():
-                writer.writerows(table)
-            page.flush_cache() # 清空缓存，否则页面过多会直接拉爆内存。
+        for table in tqdm(tables, desc="Writing into CSV: "):
+            writer.writerows(table)
 
 
 if __name__ == '__main__':
     # pdf = 'z:/1.pdf'
     parser = argparse.ArgumentParser(
-                    prog='pdf2csv.py',
-                    description='Extract tables in pdf file and save as csv file',
-                    epilog='InnerSea at 2023-3-14')
+        prog='pdf2csv.py',
+        description='Extract tables in pdf file and save as csv file',
+        epilog='InnerSea at 2023-3-14')
     parser.add_argument('pdf', help='pdf file path')
     pdf = parser.parse_args().pdf
-    pdf2csv(pdf, f"{pdf}.csv")
+    table_list_to_csv(f"{pdf}.csv", pdf_to_table_list(pdf))
